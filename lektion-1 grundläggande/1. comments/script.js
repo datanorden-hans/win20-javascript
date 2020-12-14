618b996a71a2f741b87993521c1449b2c972d5fe
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2020-12-14 13:40:49 +0100

    Lektion 1

---
+++ lektion-1 grundläggande/1. comments/script.js
@@ -0,0 +1,13 @@
+// inline comment / line comment
+// ctrl + ' 
+
+/* 
+    multi line comment / block comment
+    shift + alt + a
+*/
+
+/* 
+    Hur mycket ska jag kommentera i min JS-kod?
+    -------------------------------------------
+    Svar: lagomt mycket!
+*/
