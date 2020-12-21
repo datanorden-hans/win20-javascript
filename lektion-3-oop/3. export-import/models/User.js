20dda16614e5498366e3477ae2ab77971b93daf2
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2020-12-21 15:42:49 +0100

    Lektion 3

---
+++ lektion-3-oop/3. export-import/models/User.js
@@ -0,0 +1,13 @@
+export default class User {
+    constructor(firstname, lastname, email) {
+        this.firstName = firstname
+        this.lastName = lastname
+        this.email = email
+    }
+
+    get id() {
+        return Date.now().toString()
+    }
+}
+
+
